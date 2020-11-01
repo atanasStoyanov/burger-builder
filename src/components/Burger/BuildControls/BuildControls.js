4e6b9bc9ab185eeaa05082ef18e888a8ff8e936f
Stoyanov <atanass@dxc.com>
2020-11-01 18:24:22 +0200

    feat: order summary

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -11,7 +11,7 @@ const controls = [
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
-        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
+        <p>Current Price: <strong>{props.price.toFixed(2)} $</strong></p>
         {controls.map(ctrl => (<BuildControl
             key={ctrl.label}
             label={ctrl.label}
@@ -19,6 +19,7 @@ const buildControls = (props) => (
             removed={() => props.ingredientRemoved(ctrl.type)}
             disabled={props.disabled[ctrl.type]} />
         ))}
+        <button disabled={!props.purchasable} className={classes.OrderButton} onClick={props.ordered}>ORDER NOW</button>
     </div>
 )
 
