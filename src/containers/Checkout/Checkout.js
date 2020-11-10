8c56fc27ea6ce186b193d6af9a88bd3958a5d63e
Stoyanov <atanass@dxc.com>
2020-11-11 00:07:01 +0200

    feat: added routing

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import CheckoutSmmary from '../../components/CheckoutSummary/CheckoutSummary';
 
@@ -12,10 +12,32 @@ class Checkout extends Component {
         }
     }
 
+    componentDidMount() {
+        const query = new URLSearchParams(this.props.location.search);
+        const ingredients = {};
+
+        for (const param of query.entries()) {
+            ingredients[param[0]] = +param[1];  
+        }
+
+        this.setState({ingredients: ingredients});
+    }
+
+    checkoutCancelledHandler = () => {
+        this.props.history.goBack();
+    }
+
+    checkoutContinuedHandler = () => {
+        this.props.history.replace('/checkout/contact-data');
+    }
+
     render() {
         return (
             <div>
-                <CheckoutSmmary ingredients={this.state.ingredients}/>
+                <CheckoutSmmary
+                    ingredients={this.state.ingredients}
+                    checkoutCancelled={this.checkoutCancelledHandler}
+                    checkoutContinued={this.checkoutContinuedHandler} />
             </div>
         )
     }
