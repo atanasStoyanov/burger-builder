0e1a92bb49e386cf4637940566d7c029fd803bd1
Stoyanov <atanass@dxc.com>
2020-11-14 00:48:07 +0200

    fead: form submission and validation

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -5,26 +5,38 @@ const input = (props) => {
 
     let inputElement = null;
 
+    const inputClasses = [classes.InputElement];
+
+    if (props.invalid && props.shouldValidate && props.touched) {
+        inputClasses.push(classes.Invalid);
+    }
+
+    let validationError = null;
+
+    if (props.invalid && props.touched) {
+        validationError = <p className={classes.ValidationError}>Please enter a valid value!</p>
+    }
+
     switch (props.elementType) {
         case 'input':
             inputElement = <input
-                className={classes.InputElement}
+                className={inputClasses.join(' ')}
                 {...props.elementConfig}
-                value={props.value} 
-                onChange={props.changed}/>;
+                value={props.value}
+                onChange={props.changed} />;
             break;
         case 'textarea':
             inputElement = <textarea
-                className={classes.InputElement}
+                className={inputClasses.join(' ')}
                 {...props.elementConfig}
-                value={props.value} 
-                onChange={props.changed}/>;
+                value={props.value}
+                onChange={props.changed} />;
             break;
         case 'select':
             inputElement = (
                 <select
-                    className={classes.InputElement}
-                    value={props.value} 
+                    className={inputClasses.join(' ')}
+                    value={props.value}
                     onChange={props.changed}>
                     {props.elementConfig.options.map(option => (
                         <option key={option.value} value={option.value}>
@@ -35,7 +47,7 @@ const input = (props) => {
             break;
         default:
             inputElement = <input
-                className={classes.InputElement}
+                className={inputClasses.join(' ')}
                 {...props.elementConfig}
                 value={props.value} />;
             break;
@@ -45,6 +57,7 @@ const input = (props) => {
         <div className={classes.Input}>
             <label className={classes.Label}>{props.label}</label>
             {inputElement}
+            {validationError}
         </div>
     );
 }
