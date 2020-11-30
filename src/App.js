a31bdc5f9eddb5c7b82e1ca1abfef959cd95de94
Stoyanov <atanass@dxc.com>
2020-12-01 01:27:48 +0200

    feat: user authentication

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
+import Auth from "./containers/Auth/Auth";
 
 import { Route, Switch } from 'react-router-dom';
 
@@ -16,6 +17,7 @@ class App extends Component {
             <Route path='/checkout' component={Checkout} />
             <Route path='/orders' exact component={Orders} />
             <Route path='/' exact component={BurgerBuilder} />
+            <Route path='/auth' exact component={Auth} />
           </Switch>
         </Layout>
       </div>
